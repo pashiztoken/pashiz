97f90f9001f3d1cb087ecbd206d3befc7be134d5
pashiztoken <info@pashiz.finance>
2021-09-28 13:04:25 +0330

    First commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import { useTransition, a } from 'react-spring'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OrbitControls, draco } from 'drei'
 import logo from './logoNew.png';
+import './menu.css'
+
+
+
 
 function Model({ url }) {
   const { nodes, materials } = useLoader(GLTFLoader, url, draco());
@@ -47,23 +51,308 @@ function Loading() {
 }
 
 
+function shoot() {
+    document.getElementById("toggle").style.display="block";
+}
+
 export default function App() {
-  return (
+
+    return (
     <>
       <div className="bg" />
-
       <h1>
         Baby Doge Coin
       </h1>
         <img src={logo} alt="Logo" />
-        <nav>
-        <ul className="list-wrap">
-            <li><a href="#">Wallet</a></li>
-            <li><a href="#">Exchange</a></li>
-            <li><a href="#">NFT</a></li>
-            <li><a href="#">Arcade</a></li>
-        </ul>
-        </nav>
+        <div id="toggle" className="toggle">
+            <h5>REWARDS TRACKER</h5>
+            <p>Enter your BabyDoge wallet address below to get started.</p>
+            <div className="card-body">
+                <div className="row">
+                    <input placeholder="0xAABB..." className="input" value=""/>
+                </div>
+                <div className="row">
+                    <button type="button" className="button">START TRACKING</button>
+                </div>
+            </div>
+            <p>
+                Refreshing the page will cause it to lose your stats this data is not stored in our server, it is fetched directly from the network in real-time. This tool is for educational purposes only and we do not guarantee accurate data. Do not make decisions based off data presented. Baby Doge LLC nor any of its community or members will be responsible for any use, misuse, or decisions made from tracker
+            </p>
+        </div>
+        <div className="menu">
+            <div className="grain"></div>
+            <div className="intro">
+                <div className="center">
+                    <div className="core"></div>
+                    <div className="outer_one">
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                        <div className="outer_one__piece"></div>
+                    </div>
+                    <div className="outer_two">
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                        <div className="outer_two__piece"></div>
+                    </div>
+                    <div className="outer_three">
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                        <div className="outer_three__piece"></div>
+                    </div>
+                    <div className="outer_four">
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                        <div className="outer_four__piece"></div>
+                    </div>
+                    <div className="outer_five">
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                        <div className="outer_five__piece"></div>
+                    </div>
+                    <div className="pieces" >
+                        <div className="future_ui__piece" onClick={shoot}>
+                            <span>Track</span>
+                            <div className="line"></div>
+                            <div className="tip">
+                                Award Tracker
+                            </div>
+                        </div>
+                        <div className="future_ui__piece">
+                            <span>Wallet</span>
+                            <div className="line"></div>
+                            <div className="tip">
+                                Debit Card
+                            </div>
+                        </div>
+                        <div className="future_ui__piece">
+                            <span>Swap</span>
+                            <div className="line"></div>
+                            <div className="tip">
+                                Swap
+                            </div>
+                        </div>
+                        <div className="future_ui__piece">
+                            <span>Exchange</span>
+                            <div className="line"></div>
+                            <div className="tip">
+                                 Trade
+                            </div>
+                        </div>
+                        <div className="future_ui__piece">
+                            <span>NFT</span>
+                            <div className="line"></div>
+                            <div className="tip">
+                                 Marketplace
+                            </div>
+                        </div>
+                        <div className="future_ui__piece">
+                            <span>Game</span>
+                            <div className="line"></div>
+                            <div className="tip">
+                                Arcade
+                            </div>
+                        </div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                        <div className="future_ui__piece blank"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
       <Canvas shadowMap camera={{ position: [0, 0, 15] }}>
         <ambientLight intensity={0.75} />
         <pointLight intensity={1} position={[-10, -25, -10]} />
@@ -92,6 +381,7 @@ export default function App() {
           minPolarAngle={Math.PI / 2}
         />
       </Canvas>
+
       <div className="layer" />
 
       <Loading />
@@ -99,3 +389,6 @@ export default function App() {
     </>
   )
 }
+
+
+
